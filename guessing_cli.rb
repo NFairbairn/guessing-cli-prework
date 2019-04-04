d01a2c79ece06359e4716e8cc538de3741729c66
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-04 18:38:19 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -11,30 +11,12 @@ def run_guessing_game
       case user_input.chomp
         when comp_guess
           puts "You guessed the correct number!"
-        when "exit"
-          puts "Goodbye"
+        when 'exit'
+          puts "Goodbye!"
           break
           #else works
         else
           puts "The computer guessed #{comp_guess}."
       end
     end
-end
-
-def run_guessing_game
-  input = ""
-  while input
-    puts "Guess a number between 1 and 6."
-    input = gets.downcase.chomp
-    random_number = rand(1..6).to_s
-    case input.chomp
-    when random_number
-      puts "You guessed the correct number!"
-    when 'exit'
-      puts "Goodbye!"
-      break
-    else
-      puts "The computer guessed #{random_number}."
-    end
-  end
 end
