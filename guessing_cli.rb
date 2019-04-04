843716f3e887603cafeaca65198ed6ad7724604b
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-04 18:34:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,8 +4,9 @@ require 'pry'
 
 def run_guessing_game
   user_input = ""
+  puts "Guess a number between 1 and 6."
     while user_input
-      puts "Guess a number between 1 and 6."
+      
       user_input = gets.downcase.chomp
       comp_guess = rand(1..6).to_s
       case user_input.chomp
