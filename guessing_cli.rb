fc66657f21ff0a6760f681707a1afc8b9e8dc66e
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-04 18:24:45 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,13 +9,13 @@ def run_guessing_game
       comp_guess = rand(1..6).to_s
       user_input = gets.downcase.chomp
       case user_input
-      when "exit"
-        puts "Goodbye"
-        break
-      when comp_guess
-        puts "You guessed the correct number!"
-      else
-        puts "The computer guessed #{comp_guess}"
+        when "exit"
+          puts "Goodbye"
+          break
+        when comp_guess
+          puts "You guessed the correct number!"
+        else
+          puts "The computer guessed #{comp_guess}"
       end
     end
 end
